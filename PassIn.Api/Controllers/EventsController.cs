009ddc18425485a66008141213bed1acf2c141bb
David Soares <david.bsoares@hotmail.com>
2024-04-01 22:12:25 -0300

    Add Events Controllers

---
+++ PassIn.Api/Controllers/EventsController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PassIn.Application.UseCases.Events.GetById;
+using PassIn.Application.UseCases.Events.Register;
+using PassIn.Communication.Requests;
+using PassIn.Communication.Responses;
+using PassIn.Exceptions;
+
+namespace PassIn.Api.Controllers {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventsController : ControllerBase {
+        [HttpPost]
+        [ProducesResponseType(typeof(ResponseRegisteredEventJson), StatusCodes.Status201Created)]
+        [ProducesResponseType(typeof(ResponseErrorJson), StatusCodes.Status400BadRequest)]
+        public IActionResult Register([FromBody] RequestEventJson request) {
+            try {
+                var useCase = new RegisterEventUseCase();
+                var res = useCase.Execute(request);
+
+                return Created(string.Empty, res);
+            }
+            catch (PassInException ex) {
+                return BadRequest(new ResponseErrorJson(ex.Message));
+            }
+            catch {
+                return StatusCode(StatusCodes.Status500InternalServerError, new ResponseErrorJson("Unknown error"));
+            }
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        [ProducesResponseType(typeof(ResponseEventJson), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ResponseErrorJson), StatusCodes.Status404NotFound)]
+        public IActionResult GetById([FromRoute] Guid id) {
+            try {
+                var useCase = new GetEventByIdUseCase();
+
+                var res = useCase.Execute(id);
+
+                return Ok(res);
+            }
+            catch (PassInException ex) {
+                return NotFound(new ResponseErrorJson(ex.Message));
+            }
+            catch {
+                return StatusCode(StatusCodes.Status500InternalServerError, new ResponseErrorJson("Unknown error"));
+            }
+        }
+    }
+}
---
+++ PassIn.Application/UseCases/Events/Register/RegisterEventUseCase.cs
@@ -0,0 +1,32 @@
+﻿using PassIn.Communication.Requests;
+using PassIn.Communication.Responses;
+using PassIn.Exceptions;
+using PassIn.Infrastructure;
+
+namespace PassIn.Application.UseCases.Events.Register {
+    public class RegisterEventUseCase {
+        public ResponseRegisteredEventJson Execute(RequestEventJson request) {
+            Validate(request);
+
+            var dbContext = new PassInDbContext();
+            var entity = new Infrastructure.Entities.Event { // todo: use automapper
+                Title = request.Title,
+                Details = request.Details,
+                Maximum_Attendees = request.MaximumAttendees,
+                Slug = request.Title.ToLower().Replace(" ", "-")
+            };
+
+            dbContext.Events.Add(entity); // todo: change to AddAsync and use Task
+            dbContext.SaveChanges();
+            return new ResponseRegisteredEventJson {
+                Id = entity.Id
+            };
+        }
+
+        private void Validate(RequestEventJson request) { // todo: Use FluentValidation
+            if (request.MaximumAttendees <= 0) throw new PassInException("The Maximum attendees is invalid.");
+            if (string.IsNullOrWhiteSpace(request.Title)) throw new PassInException("The Title is required.");
+            if (string.IsNullOrWhiteSpace(request.Details)) throw new PassInException("The Title is required.");
+        }
+    }
+}
